caed0ada9603fad84045ce2df1fff2f4b5a7816d
KozZzy <mehmetcekirdekci@outlook.com>
2021-10-11 22:25:59 +0300

    Initial commit

---
+++ Bugs_N_Roses.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Bugs_N_Roses.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugs_N_Roses.Domain.Repositories
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+
+    }
+}
---
+++ Bugs_N_Roses/Repositories/OrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using Bugs_N_Roses.Domain.Entities;
+using Bugs_N_Roses.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugs_N_Roses.Infrastructure.Repositories
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+
+    }
+}
