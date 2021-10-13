a009c121db557ad65828c362a8fe5e9c54d82114
KozZzy <mehmetcekirdekci@outlook.com>
2021-10-13 15:45:57 +0300

    Product Controller yazıldı.

---
+++ Bugs_N_Roses.API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Bugs_N_Roses.Application.Services.ProductServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bugs_N_Roses.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get([FromQuery] int id)
+        {
+            var result = _productService.GetById(id);
+
+            return Ok(result);
+        }
+
+
+    }
+}
